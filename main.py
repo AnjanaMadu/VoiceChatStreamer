70332118fb8164114104b495073e672a1d8250c1
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2021-09-05 23:20:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+'''
+VoiceChatStreamer, An Telegram Bot Project
+Copyright (c) 2021 Anjana Madu <https://github.com/AnjanaMadu>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+'''
+
 import asyncio
 import pytgcalls
 import pyrogram
@@ -12,6 +30,7 @@ API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
 SESSION = os.environ.get("SESSION")
 ADMINS = os.environ.get("ADMINS")
+CHAT_ID = os.environ.get("CHAT_ID")
 vcusr = Client(SESSION, API_ID, API_HASH)
 group_call = GroupCallFactory(vcusr, GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM).get_group_call()
 
@@ -39,7 +58,6 @@ async def help_vc(client, message):
 @vcusr.on_message(filters.regex("^!urlvc"))
 async def url_vc(client, message):
     if not str(message.from_user.id) in ADMINS: return
-    CHAT_ID = message.chat.id
     try: INPUT_SOURCE = message.text.split(" ", 1)[1]
     except IndexError: return await message.reply("Give video URL")
     msg = await message.reply("__Please wait.__")
@@ -63,7 +81,6 @@ async def end_vc(client, message):
 @vcusr.on_message(filters.regex("^!ytvc (audio|video)"))
 async def yt_vc(client, message):
     if not str(message.from_user.id) in ADMINS: return
-    CHAT_ID = message.chat.id
     try:
         INPUT_SOURCE = message.text.split(" ", 2)[2]
         SOURCE_FORMAT = message.text.split(" ", 2)[1]
@@ -107,7 +124,6 @@ async def yt_vc(client, message):
 @vcusr.on_message(filters.regex("^!tgvc (audio|video)$"))
 async def tg_vc(client, message):
     if not str(message.from_user.id) in ADMINS: return
-    CHAT_ID = message.chat.id
     try: INPUT_SOURCE = message.text.split(" ", 1)[1]
     except IndexError: return await message.reply("Give file type.")
     if not message.reply_to_message: return await message.reply("Reply to video or audio")
