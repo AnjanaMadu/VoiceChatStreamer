e38d09d876053129f2dd92a6dccfbb6b6ff462c7
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2021-09-07 05:58:54 -0700

    Create __init__.py

---
+++ vcstreamer/__init__.py
@@ -0,0 +1,8 @@
+import os
+
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+TOKEN = os.environ.get("TOEKN")
+SESSION = os.environ.get("SESSION")
+ADMINS = os.environ.get("ADMINS")
+CHAT_ID = int(os.environ.get("CHAT_ID"))
