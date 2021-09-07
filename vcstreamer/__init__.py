adb424442461636e9ea80ae71b28516b0dcf3bd3
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2021-09-07 06:24:39 -0700

    Update __init__.py

--- vcstreamer/__init__.py
+++ vcstreamer/__init__.py
@@ -1,8 +1,59 @@
-import os
+'''
+VoiceChatStreamer, An Telegram Bot Project
+Copyright (c) 2021 Anjana Madu <https://github.com/AnjanaMadu>
 
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-TOKEN = os.environ.get("TOEKN")
-SESSION = os.environ.get("SESSION")
-ADMINS = os.environ.get("ADMINS")
-CHAT_ID = int(os.environ.get("CHAT_ID"))
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+'''
+
+import os
+from pyrogram import Client, filters
+from pytgcalls import GroupCallFactory
+from pytube import YouTube
+from youtube_dl import YoutubeDL
+ 
+
+class config():
+  API_ID = int(os.environ.get("API_ID"))
+  API_HASH = os.environ.get("API_HASH")
+  TOKEN = os.environ.get("TOEKN")
+  SESSION = os.environ.get("SESSION")
+  ADMINS = os.environ.get("ADMINS")
+  CHAT_ID = int(os.environ.get("CHAT_ID"))
+
+class vc_clients():
+  bot = Client(
+    "bot",
+    config.API_ID,
+    config.API_HASH,
+    bot_token=config.TOKEN,
+    plugins=dict(root="vcstreamer")
+  )
+  app = Client(
+    config.SESSION,
+    config.API_ID,
+    config.API_HASH
+  )
+
+class vctools():
+  STREAM = {8}
+  VIDEO_CALL = {}
+  group_call_factory = GroupCallFactory(vc_clients.app, GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM)
+  ydl_opts = {"geo-bypass": True, "nocheckcertificate": True}
+  ydl = YoutubeDL(ydl_opts)
+
+def video_link_getter(url: str):
+    yt = YouTube(url)
+    x = yt.streams.filter(file_extension="mp4", res="720p")[0].download("downloads")
+    return x
+  
