32882021b0981e370aa0fd2f08aa6d023fa62c05
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2021-09-08 02:52:46 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>
 '''
 
-import os, sys, asyncio, io, traceback,
+import os, sys, asyncio, io, traceback
 from pyrogram import Client, filters
 from pytgcalls import GroupCallFactory
 from pytube import YouTube
