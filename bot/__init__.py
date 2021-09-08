0ac95ef008ef74f80f8d13a815ceff3c2917dca4
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2021-09-08 09:43:41 -0700

    Create __init__.py

---
+++ bot/__init__.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+SESSION = os.environ.get("SESSION")
+
+vcusr = Client(
+    SESSION,
+    API_ID,
+    API_HASH
+)
