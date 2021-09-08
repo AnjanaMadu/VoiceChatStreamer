2562384ebf9746f48b2f5103d23d8c4e867a066f
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2021-09-08 09:47:44 -0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,4 +1,11 @@
 from bot import vcusr
 from bot.play import *
+import logging
 
-vcusr.run()
+logging.basicConfig(level=logging.WARNING,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+if __name__ == "__main__" :
+    vcusr.run()
