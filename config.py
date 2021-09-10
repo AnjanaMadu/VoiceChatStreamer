2a5427185dbd1e0fa11f26931494c4b1e07e8b76
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2021-09-10 21:04:21 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# PLEASE EDIT THIS FILE IF YOU DEPLOYING THIS BOT ON VPS. ELSE *DON'T* TOUCH THIS FILE.
+
+# Please edit 'CONFIG = False' to 'CONFIG = True'
+CONFIG = False
+
+# Add your APP ID from my.telegram.org
+API_ID = 123456
+
+# Add your API HASH from my.telegram.org
+API_HASH "xxxxxxxxxxxxxxxxxxx"
+
+# Read readme and generate string session. Then add that to here.
+SESSION = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
