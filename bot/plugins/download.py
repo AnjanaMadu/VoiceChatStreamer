ee8ba3a510e75bc918b6d6fede9a46141c6e78cf
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2021-09-12 00:05:30 +0530

    Update download.py

--- bot/plugins/download.py
+++ bot/plugins/download.py
@@ -17,12 +17,12 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>
 '''
 
 import os
-from pyrogram import filters
+from pyrogram import Client, filters
 from bot import vcusr
 from bot import yt_video_search, match_url
 import youtube_dl
 
-@vcusr.on_message(filters.command("audio", "!"))
+@Client.on_message(filters.command("audio", "!"))
 async def audio_dl(client, message):
     msg = await message.reply("⏳ __Please wait.__")
     try: INPUT_SOURCE = message.text.split(" ", 1)[1]
@@ -57,7 +57,7 @@ async def audio_dl(client, message):
         await msg.delete()
     except: pass
     
-@vcusr.on_message(filters.command("video", "!"))
+@Client.on_message(filters.command("video", "!"))
 async def video_dl(client, message):
     msg = await message.reply("⏳ __Please wait.__")
     try: INPUT_SOURCE = message.text.split(" ", 1)[1]
