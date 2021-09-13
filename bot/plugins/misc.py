c89448438512ea7d01ddf3d5a1001ffc91c8635f
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2021-09-13 15:06:06 +0530

    Update misc.py

--- bot/plugins/misc.py
+++ bot/plugins/misc.py
@@ -36,21 +36,25 @@ async def leave_vc(client, message):
     
 @Client.on_message(filters.command("pause", "!"))
 async def pause_vc(client, message):
+    global vc_paused
     if not message.chat.id == CHAT_ID: return
     if not message.from_user.id in ADMINS: return
     if vc_paused is False:
         await group_call.set_pause(True)
         await message.reply("__VC Paused!__")
+        vc_paused = True
     elif vc_paused is True:
         return await message.reply("__Already Paused.__")
           
 @Client.on_message(filters.command("resume", "!"))
 async def resume_vc(client, message):
+    global vc_paused
     if not message.chat.id == CHAT_ID: return
     if not message.from_user.id in ADMINS: return
     if vc_paused is True:
         await group_call.set_pause(False)
         await message.reply("__VC Resumed!__")
+        vc_paused = False
     elif vc_paused is False:
         return await message.reply("__VC not Paused.__")
 
