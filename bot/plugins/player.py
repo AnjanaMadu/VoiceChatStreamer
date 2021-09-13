79b8b84ce2e1a325a6568d3bd6e03094b4b94fae
Anjana Madu <86356062+AnjanaMadu@users.noreply.github.com>
2021-09-13 14:56:50 +0530

    Update player.py

--- bot/plugins/player.py
+++ bot/plugins/player.py
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>
 '''
 
-import os, asyncio, pafy
+import os, asyncio, pafy, youtube_dl
 from pyrogram import Client, filters
 from pytgcalls import GroupCallFactory
 from bot import video_link_getter, yt_video_search, match_url
