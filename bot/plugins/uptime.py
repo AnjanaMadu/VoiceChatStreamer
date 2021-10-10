932db49005fbe4b222bda0e9297ae475c127ca9e
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-10-10 19:20:02 +0530

    Will look better
    Fixed

---
+++ bot/plugins/uptime.py
@@ -0,0 +1,47 @@
+from time import time
+from datetime import datetime
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+ )
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+    
+    @Client.on_message(filters.command("ping"))
+async def ping_pong(client, m: Message):
+    start = time()
+    m_reply = await m.reply_text("pinging...")
+    delta_ping = time() - start
+    await m_reply.edit_text(
+        "🏓 `PONG!!`\n"
+        f"⚡️ `{delta_ping * 1000:.3f} ms`"
+    )
+
+
+@Client.on_message(filters.command("uptime"))
+async def get_uptime(client, m: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await m.reply_text(
+        "🤖 bot status:\n"
+        f"• **uptime:** `{uptime}`\n"
+        f"• **start time:** `{START_TIME_ISO}`"
+    )   
