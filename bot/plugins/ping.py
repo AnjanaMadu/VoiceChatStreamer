24e2b28553d95c1f19a05483f4147bce1ebbf15d
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-10-10 10:16:01 +0530

    Will look better

---
+++ bot/plugins/ping.py
@@ -0,0 +1,36 @@
+from time import time
+from datetime import datetime
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+@Client.on_message(filters.command("ping"))
+async def ping_pong(client, m: Message):
+    start = time()
+    m_reply = await m.reply_text("Pinging...")
+    delta_ping = time() - start
+    await m_reply.edit_text(
+        "🏓 `PONG!!`\n"
+        f"⚡️ `{delta_ping * 1000:.3f} ms`"
+    )
+
+
+@Client.on_message(filters.command("uptime"))
+async def get_uptime(client, m: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await m.reply_text(
+        "🤖 Bot Status:\n"
+        f"• **Uptime:** `{uptime}`\n"
+        f"• **Start Time:** `{START_TIME_ISO}`"
+    )   
